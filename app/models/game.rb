06ae42f9dd0cd375c970cd61fb99266b55980002
SilentMoon-20FH <1793012807@qq.com>
2018-12-05 10:20:53 +0000

    在各model中添加has_many关系

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,6 @@
 class Game < ApplicationRecord
+    has_many :relation_game_tags
+    has_many :tags, :through => :relation_game_tags
+    
+    has_many :comments
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,4 @@
 class Tag < ApplicationRecord
+    has_many :relation_game_tags
+    has_many :games, :through => :relation_game_tags
 end
