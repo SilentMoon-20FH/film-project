f8ce4917f33f6461d6341dcdd38d842b7a8a80fb
SilentMoon-20FH <1793012807@qq.com>
2018-12-07 06:34:03 +0000

    为tags表添加integer类型的评分字段score

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181207062728) do
+ActiveRecord::Schema.define(version: 20181207063228) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 20181207062728) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "score"
   end
 
   create_table "users", force: :cascade do |t|
