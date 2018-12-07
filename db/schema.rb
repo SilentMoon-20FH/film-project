0ab691ce3b6b24410628e997f3e4247bc82c4208
SilentMoon-20FH <1793012807@qq.com>
2018-12-07 06:27:11 +0000

    为comments表添加integer类型的评分字段score

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181206074142) do
+ActiveRecord::Schema.define(version: 20181207062340) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20181206074142) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "score"
     t.index ["game_id"], name: "index_comments_on_game_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
